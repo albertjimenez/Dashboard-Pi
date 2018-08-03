5a31351521344594dc8a32ebefd73518af94ace7
Beruto <beruto000@gmail.com>
2018-08-03 12:00:28 +0200

    cpu card

---
+++ src/app/API.ts
@@ -0,0 +1,5 @@
+// const API_ROUTE = 'localhost:5000/api/';
+const API_ROUTE = 'http://raspberryci.ddns.net:5000/api/';
+export const CPU_ROUTE = API_ROUTE + 'cpu_info';
+export const HDD_ROUTE = API_ROUTE + 'hdd_info';
+export const RAM_ROUTE = API_ROUTE + 'ram_info';
